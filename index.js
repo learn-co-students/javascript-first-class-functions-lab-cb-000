d1b09ae6ebfd8151b929b9c104a1dcb9ef06bd44
Patrick Townley <patricktownley@gmail.com>
2017-04-05 03:36:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function countdown(callback){
+  window.setTimeout(callback, 2000);
+}
+
+
+function createMultiplier(multiplierValue){
+  return function(value){
+    return value * multiplierValue;
+  }
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function multiplier(multiplierValue, value){
+  return value * multiplierValue;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
