1ccdfd6d71608aeac98a828ec6db8fc7d3f11ae1
Josef Butts <jbutts@utexas.edu>
2017-06-07 21:54:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-
-
-function countdown(callback){
-  window.setTimeout(callback, 2000)
+//excercise #!
+function countdown(callback) {
+   window.setTimeout(callback, 2000);
 }
+
+//playing scrabble exercise #2
+function createMultiplier(multiplierValue){
+  return function(value){
+    return multiplierValue * value;
+
+  };
+}
+//exercise #3 it's part(y)(ial) time
+function multiplier(arg1, arg2){
+  return arg1 * arg2;
+}
+var doublerWithBind = multiplier.bind(2, null);
+var triplerWithBind = multiplier.bind(3, null);
+
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
