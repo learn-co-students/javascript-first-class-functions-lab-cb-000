87f340dd3b9d49d2631ed888075d04b2a3abb824
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-08-04 11:26:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000)
+}
+
+function createMultiplier(multiplierValue) {
+  return (val) => {return val * multiplierValue};
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function multiplier(x, y) {
+  return x * y
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
