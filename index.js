a56386f5d87cd9e8461f80c18b5d148b27250cb4
Alex Segers <a.segers.dev@gmail.com>
2018-03-07 06:19:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 function countdown(callback) {
-  setTimeout(callback, 2000);
+  setTimeout(function () {
+    callback();
+  }, 2000);
 }
 
-var doubler = n => { return n * 2}
-var tripler = n => { return n * 3}
-
-function createMultiplier(multiplierValue) {
-  if (multiplierValue === 2) {
-    return doubler
-  } else if (multiplierValue === 3) {
-    return tripler
-  }
+function createMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
 }
 
-
-var doublerWithBind = doubler
-var triplerWithBind = tripler
-
-function multiplier(multiplierValue, value) {
-  if (multiplierValue === 2) {
-    return doublerWithBind(value)
-  } else if (multiplierValue === 3) {
-    return triplerWithBind(value)
-  }
+function multiplier(a, b) {
+  return a * b;
 }
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
